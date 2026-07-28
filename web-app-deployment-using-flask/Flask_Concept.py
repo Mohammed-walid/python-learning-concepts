adbf7dc7a0ce09d2c646e40048cab53b8bd078cd
Mohamed <mohamedwalid.elkholy@gmail.com>
2026-07-29 02:53:31 +0300

    A new module called Rendering_Templates.py for rendering and updated the import text

--- web-app-deployment-using-flask/Flask_Concept.py
+++ web-app-deployment-using-flask/Flask_Concept.py
@@ -1,11 +1,15 @@
 """this module is from using flask to run it"""
 
-import flask
-my_app=flask.Flask(__name__)
+from flask import Flask, request, render_template
+
+my_app=Flask(__name__)
 @my_app.route("/")
 def hello_world():
+    """this function is to display the home page"""
     return "Hello World"
 
 @my_app.route("/hello")
 def hello():
+    """this function is to display the page /hello"""
     return "Hello"
+
