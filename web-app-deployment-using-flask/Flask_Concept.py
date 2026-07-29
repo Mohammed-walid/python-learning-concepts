3be347e8459e49e3907fc5d1d4219cdee6b56baf
Mohamed <mohamedwalid.elkholy@gmail.com>
2026-07-29 03:08:08 +0300

    rendering

--- web-app-deployment-using-flask/Flask_Concept.py
+++ web-app-deployment-using-flask/Flask_Concept.py
@@ -13,3 +13,18 @@ def hello():
     """this function is to display the page /hello"""
     return "Hello"
 
+@my_app.route("/sample/")
+def get_sample_html():
+    return render_template("sample.html")
+
+
+@my_app.route("/user/<username>", methods=["GET"])
+def greet_user(username):
+    return render_template("result.html", username=username)
+
+
+@my_app.route("/user", methods=["GET"])
+def greet_user_based_on_req():
+    user_name = request.args.get("username")
+    return render_template("result.html", username=user_name)
+
--- web-app-deployment-using-flask/main.py
+++ web-app-deployment-using-flask/main.py
@@ -1,3 +1,3 @@
+"""Main module to run flask"""
 from Flask_Concept import my_app
-
 my_app.run(debug=True)
