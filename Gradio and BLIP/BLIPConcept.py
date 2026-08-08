ebb80230ac19372e62e8bbc1c40094dfe32a1798
Mohamed <mohamedwalid.elkholy@gmail.com>
2026-08-08 15:03:30 +0300

    Just implemented BlipForQuestionAnswering, and it actually answered the question i asked it! and also created a module for Gradio concept

--- Gradio and BLIP/BLIPConcept.py
+++ Gradio and BLIP/BLIPConcept.py
@@ -4,20 +4,20 @@ from transformers import BlipProcessor, BlipForConditionalGeneration, BlipForQue
 from PIL import Image
 
 print("downloading the processor...")
-processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
-
+processor = BlipProcessor.from_pretrained("Salesforce/blip-vqa-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-vqa-base")
+model_two = BlipForQuestionAnswering.from_pretrained("Salesforce/blip-vqa-base")
 print("Opening image...")
 image = Image.open("RDR2.png")
 
-question = "Whats this image?"
+question = "whats the man riding and whats the colour of this horse?"
 
 print("Processing image...")
 inputs = processor(image,question , return_tensors="pt")
 
 
 print("Generating captions...")
-outputs = model.generate(**inputs, max_new_tokens=40)
+outputs = model_two.generate(**inputs, max_new_tokens=40)
 caption = processor.decode(outputs[0], skip_special_tokens=True)
 
 print("Generated Caption:", caption)
--- Gradio and BLIP/__init__.py
+++ Gradio and BLIP/__init__.py
@@ -1,1 +1,2 @@
 from . import BLIPConcept
+from . import GradioConcept
